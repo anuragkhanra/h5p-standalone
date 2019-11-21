1c248928c2a9f45ff9c350cf71dc8e3fb4127444
Anurag <anuragkhanra@gmail.com>
2019-11-21 20:39:54 +0530

    plugin enhancement

--- admin/partials/wp-stand-admin-display.php
+++ admin/partials/wp-stand-admin-display.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Provide a admin area view for the plugin
  *
@@ -12,21 +11,115 @@
  * @subpackage Wp_Stand/admin/partials
  */
 ?>
-
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<?php
+$host = $_SERVER['HTTP_HOST'];
+$con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+// Process Map
+if(isset($_POST['map_submit'])){
+    $id = $_POST['id'];
+    $course = $_POST['s_course'];
+    $chapter = $_POST['s_chap'];
+    $cdate = date("d.m.Y");
+    $query = "INSERT INTO `c_map` (`wp_cont_id`,`crs_id`,`chp_id`,`map_date`) VALUES ('$id','$course','$chapter','$cdate')";
+    mysqli_query($con,$query);
+}
+?>
 <!DOCTYPE html>
 <html>
     <body>
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <form method="POST" action="">
+                <input type="hidden" name="id" id="input1">
+                Select Course: 
+                <select name="s_course" required>
+                    <option value="">__SELECT__</option>
+            <?php
+            $result2 = mysqli_query($con,"SELECT * FROM `course`");
+            while($row2 = mysqli_fetch_assoc($result2)){
+                echo '<option value="'.$row2['crs_id'].'">'.$row2['crs_name'].'</option>';
+            }
+            ?>
+                </select><br>
+                Select Chapter:
+                <select name="s_chap" required>
+                    <option value="">__SELECT__</option>
+            <?php
+            $result3 = mysqli_query($con,"SELECT * FROM `chapter`");
+            while($row3 = mysqli_fetch_assoc($result3)){
+                echo '<option value="'.$row3['chp_id'].'">'.$row3['chp_name'].'</option>';
+            }
+            ?>
+                </select><br>
+                <input type="submit" name="map_submit" value="Process Map">
+            </form>
+        </div>
+    </div>
+
+    <div id="di">
+        <?php
+        //global $current_user;
+        //get_currentuserinfo();
+        //echo $current_user->user_login;
+        
+        // Chapter and Course Creation
+            if(isset($_POST['submit_course'])){
+                $course = stripslashes($_REQUEST['course']);
+                $course = mysqli_real_escape_string($con,$course);
+                global $current_user;
+                wp_get_current_user();
+                $user = $current_user->user_login;
+                $cdate = $udate = date("d.m.Y");
+                mysqli_query($con,"INSERT INTO `course` (`crs_name`,`crs_date`,`crs_user`,`crs_update`,`crs_delete`)
+                 VALUES ('$course','$cdate','$user','$udate',1)");
+            }
+
+            if(isset($_POST['submit_chapter'])){
+                $chapter = stripslashes($_REQUEST['chp']);
+                $chapter = mysqli_real_escape_string($con,$chapter);
+                global $current_user;
+                get_currentuserinfo();
+                $user = $current_user->user_login;
+                $cdate = $udate = date("d.m.Y");
+                $crs = $_REQUEST['sitem'];
+                mysqli_query($con,"INSERT INTO `chapter` (`crs_id`,`chp_name`,`chp_date`,`chp_user`,`chp_update`,`chp_delete`) 
+                VALUES ('$crs','$chapter','$cdate','$user','$udate',1)");
+            }
+        ?>
+        <form action="" method="POST" id="f1">
+            <input type="text" name="course" value="Course Name" required>
+            <input type="submit" name="submit_course" value="New Course">
+        </form>
+        <form action="" method="POST" id="f2">
+        <select name="sitem" required>
+        <option value="">__SELECT__</option>
+            <?php
+            $result1 = mysqli_query($con,"SELECT * FROM `course`");
+            while($row1 = mysqli_fetch_assoc($result1)){
+                echo '<option value="'.$row1['crs_id'].'">'.$row1['crs_name'].'</option>';
+            }
+            ?>
+        </select>
+            <input type="text" name="chp" value="Chapter Name" required>
+            <input type="submit" name="submit_chapter" value="New Chapter">
+        </form>
+    </div>
+
+    <!-- unmapped Table -->
+
     <table border="2px" style="width:70%; text-align: center; margin: 35px">
+        <thead><tr><th colspan="4" style="font-size:145%;font-weight: bold">Unmapped Contents</th></tr></thead>
         <tr style="font-size:125%;font-weight: bold">
-            <td>ID</td><td>Title</td><td>Initials</td><td>Process</td>
+            <td>ID</td><td>Title</td><td>Initials</td><td>Map</td>
         </tr>
 <?php
 require 'ex.php';
-    $host = $_SERVER['HTTP_HOST'];
-    $con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
     $root_dir = $_SERVER['DOCUMENT_ROOT']."/wordpress/wp-content/plugins/wp-stand/admin/partials/" ;
-    if(isset($_POST['submit'])){
+
+    // Process form
+    if(isset($_POST['submit_process'])){
         //download the package
         $slug = $_REQUEST['slug'];
         $id = $_REQUEST['id'];
@@ -68,10 +161,44 @@ require 'ex.php';
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 
-    $query = "SELECT id, title, slug FROM `wp_h5p_contents`";
+    $query1 = "SELECT wp_h5p_contents.id, wp_h5p_contents.title, wp_h5p_contents.slug FROM `wp_h5p_contents`
+    INNER JOIN c_map ON wp_h5p_contents.id=c_map.wp_cont_id";
+    $query = "SELECT id, title, slug FROM `wp_h5p_contents` WHERE id NOT IN (SELECT wp_cont_id FROM c_map)";
     $result = mysqli_query($con,$query);
     if(mysqli_num_rows($result)==0){
-        echo '<tr><td colspan="4">No contents yet</td></tr>';
+        echo '<tr><td colspan="6">No contents yet</td></tr>';
+    }
+    else{
+        while($row = mysqli_fetch_assoc($result)){
+            ?>
+            <?php
+            echo "<tr><td>{$row['id']}</td><td>{$row['title']}</td><td>{$row['slug']}</td></td>";
+            ?>
+                <?php
+                    echo "<td><button onclick='myF(".$row['id'].")'>Map</button></td>";
+                ?>
+            <?php
+        }
+    }
+?>
+
+    </table>
+
+    <!-- Mapped Table -->
+
+    <table border="2px" style="width:70%; text-align: center; margin: 35px">
+        <thead><tr><th colspan="4" style="font-size:145%;font-weight: bold">Mapped Contents</th></tr></thead>
+        <tr style="font-size:125%;font-weight: bold">
+            <td>ID</td><td>Title</td><td>Initials</td><td>Process</td>
+        </tr>
+<?php
+
+    $query1 = "SELECT wp_h5p_contents.id, wp_h5p_contents.title, wp_h5p_contents.slug FROM `wp_h5p_contents`
+    INNER JOIN c_map ON wp_h5p_contents.id=c_map.wp_cont_id";
+    //$query = "SELECT id, title, slug FROM `wp_h5p_contents` WHERE id NOT IN (SELECT wp_cont_id FROM c_map)";
+    $result = mysqli_query($con,$query1);
+    if(mysqli_num_rows($result)==0){
+        echo '<tr><td colspan="6">No contents yet</td></tr>';
     }
     else{
         while($row = mysqli_fetch_assoc($result)){
@@ -81,7 +208,7 @@ require 'ex.php';
             echo "<tr><td>{$row['id']}</td><td>{$row['title']}</td><td>{$row['slug']}</td>
             <input type='hidden' name='id' value='".$row['id']."'>
             <input type='hidden' name='slug' value='".$row['slug']."'>
-            <td><input type='submit' name='submit' value='Process'></td></tr>\n";
+            <td><input type='submit' name='submit_process' value='Process'></td>";
             ?>
             </form>
             <?php
@@ -90,5 +217,34 @@ require 'ex.php';
 ?>
 
     </table>
+    
+    <script>
+    var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+function myF(id){
+    modal.style.display = "block";
+    document.getElementById("input1").value = id;
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+    </script>
     </body>
 </html>
